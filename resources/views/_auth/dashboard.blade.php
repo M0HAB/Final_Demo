76dbf1bea1412d7ad094bb0ae35a7870418f8244
Mohab <mohab@example.com>
2018-02-18 18:42:42 +0200

    mod: user dashboard

--- resources/views/_auth/dashboard.blade.php
+++ resources/views/_auth/dashboard.blade.php
@@ -7,29 +7,27 @@
 	<div class="content mt-5 mb-4">
 		<div class="container">
 			<div class="row">
-				<div class="col-lg-8 mb-4">
+				<div class="col-lg-8 col-sm-12 mb-4">
                     <div class="user-dashboard-diss-fourm-prev">
-                        <h1 class="display-4">{{ Auth::user()->fname . ' ' . Auth::user()->lname }}</h1>
+                        <h1 class="display-4">{{ Auth::user()->fname }}</h1>
                         <div class="bbp-breadcrumb pb-4">
                             <p>
                                 <a href="http://demo.vellumwp.com/" class="bbp-breadcrumb-home">Home</a> 
                                 <span class="bbp-breadcrumb-sep">&rsaquo;</span> 
-                                <span class="bbp-breadcrumb-current">Forums</span>
+                                <span class="bbp-breadcrumb-current">Dashboard</span>
                             </p>
                         </div>
-					
-					    <form action="">
-					        <table class="table table-hover">
-                                <thead>
-                                  <tr>
-                                    <th>Discussion Forum</th>
-                                    <th>Topics</th>
+						<table class="table table-hover">
+							<thead>
+								<tr>
+									<th>Discussion Forum</th>
+									<th>Topics</th>
 									<th>Posts</th>                                             
 									<th>Recent Post</th>
-                                  </tr>
-                                </thead>
-                                <tbody>
-                                  <tr>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
                                     <td>
                                         <a href="#" class="font-weight-bold forum-nav">Artificial Intelligence</a>
                                         <p class="text-muted pt-1 forum-p">If it is football, hiking or whatever: Activities go here</p>
@@ -43,8 +41,8 @@
                                     <td>
                                         <span class="text-muted forum-recent-time">4 years, 1 month ago</span>
                                     </td>
-                                  </tr>
-                                  <tr>
+								</tr>
+								<tr>
                                     <td>
                                         <a href="#" class="font-weight-bold forum-nav">Computer Modelling And Simulation</a>
                                         <p class="text-muted pt-1 forum-p">If it is football, hiking or whatever: Activities go here</p>
@@ -58,8 +56,8 @@
                                     <td>
                                         <span class="text-muted forum-recent-time">4 years, 1 month ago</span>
                                     </td>
-                                  </tr>
-                                  <tr>
+								</tr>
+								<tr>
                                     <td>
                                         <a href="#" class="font-weight-bold forum-nav">Digital Signal Processing</a>
                                         <p class="text-muted pt-1 forum-p">If it is football, hiking or whatever: Activities go here</p>
@@ -73,8 +71,8 @@
                                     <td>
                                         <span class="text-muted forum-recent-time">4 years, 1 month ago</span>
                                     </td>
-                                  </tr>
-                                  <tr>
+								</tr>
+								<tr>
                                     <td>
                                         <a href="#" class="font-weight-bold forum-nav">Pattern Recognition</a>
                                         <p class="text-muted pt-1 forum-p">If it is football, hiking or whatever: Activities go here</p>
@@ -88,63 +86,47 @@
                                     <td>
                                         <span class="text-muted forum-recent-time">4 years, 1 month ago</span>
                                     </td>
-                                  </tr>
-                                </tbody>
-                              </table>
-					    </form>
+								</tr>
+							</tbody>
+						</table>
 					</div>
 				</div>
-				<div class="col-lg-4">
+				<div class="col-lg-4 col-sm-12">
 					<h4 class="mb-4 pl-2">Recent Topics</h4>
-					<div class="row mb-2 pl-2">
-						<div class="col-lg-1">
-							<i class="fas fa-angle-right d-none d-lg-block"></i>
-						</div>
-						<div class="col-lg-11 mb-2" style="border-bottom: 1px solid #DEE2E6">
-							<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta, nam.</p>
-						</div>
+					<div class="list-group">
+						<a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+							<div class="d-flex w-100 justify-content-between">
+								<h5 class="mb-2">Lorem ipsum dolor sit amet.</h5>
+								<small class="text-muted txt-lbl">3 days ago</small>
+							</div>
+							<p class="mb-1 recent-p-mod">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae, quia.</p>
+							<small class="text-muted txt-lbl">Posted by: <span class="font-weight-bold">Mohab Hamdy</span></small>
+						</a>
+						<a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+							<div class="d-flex w-100 justify-content-between">
+								<h5 class="mb-2">Lorem ipsum dolor sit amet.</h5>
+								<small class="text-muted txt-lbl">3 days ago</small>
+							</div>
+							<p class="mb-1">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae, sit!</p>
+							<small class="text-muted txt-lbl">Posted by: <span class="font-weight-bold">Mohab Hamdy</span></small>
+						</a>
+						<a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+							<div class="d-flex w-100 justify-content-between">
+								<h5 class="mb-2">Lorem ipsum dolor sit amet.</h5>
+								<small class="text-muted txt-lbl">3 days ago</small>
+							</div>
+							<p class="mb-1">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, temporibus.</p>
+							<small class="text-muted txt-lbl">Posted by: <span class="font-weight-bold">Mohab Hamdy</span></small>
+						</a>
+						<a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+							<div class="d-flex w-100 justify-content-between">
+								<h5 class="mb-2">Lorem ipsum dolor sit amet.</h5>
+								<small class="text-muted txt-lbl">3 days ago</small>
+							</div>
+							<p class="mb-1">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, temporibus.</p>
+							<small class="text-muted txt-lbl">Posted by: <span class="font-weight-bold">Mohab Hamdy</span></small>
+						</a>
 					</div>
-					<div class="row mb-2 pl-2">
-						<div class="col-lg-1">
-							<i class="fas fa-angle-right d-none d-lg-block"></i>
-						</div>
-						<div class="col-lg-11 mb-2" style="border-bottom: 1px solid #DEE2E6">
-							<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta, nam.</p>
-						</div>
-					</div>
-					<div class="row mb-2 pl-2">
-						<div class="col-lg-1">
-							<i class="fas fa-angle-right d-none d-lg-block"></i>
-						</div>
-						<div class="col-lg-11 mb-2" style="border-bottom: 1px solid #DEE2E6">
-							<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta, nam.</p>
-						</div>
-					</div>
-					<div class="row mb-2 pl-2">
-						<div class="col-lg-1">
-							<i class="fas fa-angle-right d-none d-lg-block"></i>
-						</div>
-						<div class="col-lg-11 mb-2" style="border-bottom: 1px solid #DEE2E6">
-							<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta, nam.</p>
-						</div>
-					</div>
-					<div class="row mb-2 pl-2">
-						<div class="col-lg-1">
-							<i class="fas fa-angle-right d-none d-lg-block"></i>
-						</div>
-						<div class="col-lg-11 mb-2" style="border-bottom: 1px solid #DEE2E6">
-							<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta, nam.</p>
-						</div>
-					</div>
-					<div class="row mb-2 pl-2">
-						<div class="col-lg-1">
-							<i class="fas fa-angle-right d-none d-lg-block"></i>
-						</div>
-						<div class="col-lg-11 mb-2" style="border-bottom: 1px solid #DEE2E6">
-							<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta, nam.</p>
-						</div>
-					</div>
-					
 				</div>
 			</div>
 		</div>
