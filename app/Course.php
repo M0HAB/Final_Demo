0058328a17fbb996daf73585136298c280c3edae
Mostafa Gamal <mostafagamal99418@gmail.com>
2018-03-03 16:09:59 +0200

    Make Course model

---
+++ app/Course.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Course extends Model
+{
+    protected $fillable = [
+        'code', 'title', 'description', 'course_language', 'start_date', 'end_date',
+        'course_specialization',  'commitment', 'course_department',
+        'how_to_pass','instructor_id'
+    ];
+}
