4c4cbf8cb7b6602991f849d656025e5090d9b0e9
Mohab <mohab.hamdy123@gmail.com>
2018-06-05 12:36:42 +0200

    Add plugins to local based

--- resources/views/_layouts/app.blade.php
+++ resources/views/_layouts/app.blade.php
@@ -12,11 +12,11 @@
     
     <!-- ===============/ Scripts /=============== -->
     <!-- jQuery library -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <!-- Popper JS -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="{{ asset('js/jquery.min.js') }}"></script>
+    <!--  Popper-js -->
+    <script src="{{ asset('js/popper.min.js') }}"></script>
     <!-- Latest compiled JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
     <!-- Font-awesome -->
     <script src="{{ asset('js/fontawesome-all.min.js') }}"></script>
     <!-- Toastr -->
