509fdcdf893b184c1ec2fdad403f937f910ac040
Oblivionist <darshoka705@gmail.com>
2018-06-20 21:38:57 +0200

    navbarfix

--- resources/views/_partials/_navbar.blade.php
+++ resources/views/_partials/_navbar.blade.php
@@ -4,7 +4,7 @@
     <div class="container">
 
         @if (Auth::check())
-            <a class="navbar-brand" href="{{ route('user.dashboard') }}"><img width="150px" height="35px" class="mr-1 "src="/images/logo4.png"></a>
+            <a class="navbar-brand" href="{{ route('user.dashboard') }}"><img width="150px" height="35px" class="mr-1" src="/images/logo4.png"></a>
         @else
             <a class="navbar-brand" href="{{ route('index') }}"><img width="150px" height="35px" class="mr-1" src="/images/logo4.png"></a>
         @endif
